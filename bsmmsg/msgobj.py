3560fdeeacbbc58256443162e2ab27c7739d06a1
Przemek Sekula <przemeksekula@gmail.com>
2020-04-25 19:19:11 +0200

    refactor: New structure of the code
    - bsmmsg/msgobj.py added
    - bsmserver.py is a normal .py file now (not .ipynb)
    - new parameters added

---
+++ bsmmsg/msgobj.py
@@ -0,0 +1,128 @@
+from collections import OrderedDict
+import json
+from time import time
+
+class MsgObj():
+    """
+	Each MsgObj contains one BSM message
+	Object variables:
+		id        - id of the object
+		active    - Active flag. Only active objects are being sent to clients.
+		            When the object is sent, the flag is being set to False. The
+				    next update resets the flag to True
+		lastRead  - the last time when the object was sent
+		timestamp - object's timestamp
+		msg       - the BSM message in json format
+	"""
+    def __init__(self, obj_id, timestamp, msg, active = True, lastRead = 0):
+        self.id = obj_id,
+        self.active = active # Only active objects are being sent
+        self.lastRead = lastRead # When the object was read last time
+        self.timestamp = timestamp
+        self.msg = msg
+
+    def dumpObj(self):
+        print ('Id: {}, Active: {}, Last Read: {}, Timestamp: {}\nMsg: {}'.format(
+			self.id, self.active, self.lastRead, self.timestamp, self.msg))
+        
+        
+class MsgObjects():
+    """
+	Class that manages the Message Objects. The main methods of this class are:
+	push_object - receives the BSM(s) (usually from detaction and tracking system)
+	pull_object - receives request and sends the BSM (usually to Cohda)
+	get_bsms    - returns all the BSMs (usually used by an externall app)
+	Object variables:
+	    obj_lifetime - lifetime of an object (default = 20)
+		objects - OrderedDict({
+		              id : MsgObj
+		              .......
+				  })
+	              where id is an object id and MsgObj is an instance of MsgObj class
+	"""			  
+	
+    def __init__(self, obj_lifetime = 20):
+        self.objects = OrderedDict()
+        self.obj_lifetime = obj_lifetime
+    
+    def first(self):
+        if len(self.objects) == 0:
+            return None
+        return next(self.objects)
+    
+    def sort_objects(self):
+        self.objects = OrderedDict(sorted(
+            self.objects.items(), key=lambda item: item[1].lastRead))
+        
+    def push_object(self, msg, force_sort = False):
+        if isinstance(msg, str):
+            msg_dict = json.loads(msg)
+        elif isinstance(msg, dict):
+            msg_dict = msg
+            msg = json.dumps(msg_dict)
+            
+        if 'id' in msg_dict:
+            objid = msg_dict['id']
+            if 'secMark' in msg_dict:
+                timestamp = msg_dict['secMark']
+            else:
+                timestamp = time()
+            if objid in self.objects.keys():
+                self.objects[objid].msg = msg
+                self.objects[objid].timestamp = timestamp
+                self.objects[objid].active = True
+            else:
+                obj = MsgObj(objid, timestamp, msg)
+                self.objects[obj.id] = obj
+        else:
+            return 1 # Error - cannot push the object
+        
+        if force_sort:
+            self.sort_objects()
+        return 0
+
+    def pull_object(self, force_sort = False):
+        if len(self.objects) == 0:
+            return None
+        if force_sort:
+            self.sort_objects()
+        key, obj = self.objects.popitem(last = False)
+        
+        # Recurrence. If our object is too old, let's take the next one. 
+        # Current object is already removed from the list by popitem
+        if time() - obj.timestamp > self.obj_lifetime:
+            return (self.pull_object(False))
+        
+        
+        self.objects[key] = obj
+        if not obj.active:
+            return None
+        obj.lastRead = time()
+        obj.active = False
+        return obj
+    
+    
+    def pull_objects(force_sort = False):
+        if force_sort:
+            self.sort_objects()
+        ret = copy.deepcopy(self.objects)
+        self.objects = OrderedDict()
+        return ret
+    
+    def pull_bsm(self, force_sort = False):
+        obj = self.pull_object(force_sort)
+        if obj is None:
+            return '{"msg" : ""}'
+        return obj.msg
+    
+    def get_bsms(self, force_sort = False):
+        """
+        Returns bsm msgs for all objects
+        Arguments: force_sort - if true, then objects are sorted first (default = False)
+        Returns:   list with bsm msgs in json format
+        """
+        if force_sort:
+            self.sort_objects()
+        return [x.msg for x in self.objects.values()]
+        
+    
---
+++ bsmserver.py
@@ -0,0 +1,135 @@
+import argparse
+
+import os
+import sys
+
+import socket
+import threading
+import json
+
+from time import time
+from bsmmsg.msgobj import MsgObjects
+
+parser = argparse.ArgumentParser(description='Runs the BSM server')
+
+parser.add_argument('--host',
+                    type=str,
+                    default='127.0.0.1',
+                    help='IP of the host with the BSM server. Default=127.0.0.1'
+                   )
+
+parser.add_argument('-p', '--port',
+                    type=int,
+                    default=65432,
+                    help='BSM server port. Default=65432'
+                   )
+
+parser.add_argument('--data_buffer',
+                    type=int,
+                    default=4096,
+                    help='Size of data buffer in bytes. Default=4096'
+                   )
+
+parser.add_argument('--object_lifetime',
+                    type=int,
+                    default=20,
+                    help='Object lifetime in seconds. Default=20'
+                   )
+
+parser.add_argument('--end_thread_time',
+                    type=int,
+                    default=10,
+                    help="end a thread if didn't receive anything for X seconds. Default=10"
+                   )
+
+parser.add_argument('--ok',
+                    type=str,
+                    default='Ok.',
+                    help="Ok message. Default=Ok."
+                   )
+
+parser.add_argument('-v', '--verbose',
+                    type=int,
+                    default=0,
+                    help="Verbose for debugging purposes"
+                   )
+
+
+def push_data(request):
+    global msgObjects
+    json_msgs = json.loads(request['msg'])
+    if args.verbose >= 2:
+        print ("json_msgs:", type(json_msgs), json_msgs)
+    if isinstance(json_msgs, dict):
+        json_msgs = [json_msgs]
+    ret = sum([msgObjects.push_object(json_msg) for json_msg in json_msgs])
+    msgObjects.sort_objects()
+    if ret > 0:
+        return "ERROR: Failed to push {} / {} objects.".format(ret, len(json_msgs))
+    return args.ok
+
+
+def pull_data(conn, request):
+    myMsg = msgObjects.pull_bsm()
+    if args.verbose >=1:
+        print ("PULL:", myMsg)
+    myMsg = str.encode(myMsg)
+    conn.sendall(myMsg)
+
+
+def check_data(conn, request):
+    myMsg = {'msgs': msgObjects.get_bsms()}
+    myMsg = json.dumps(myMsg)
+    myMsg = str.encode(myMsg)
+    conn.sendall(myMsg)
+
+
+def connection(nr, conn, addr):
+    last_received = time()
+    global buff
+    global counter
+    print ("Connection nr: {}, c: {}, addr: {}".format(nr, conn, addr))
+    while True:
+        data = conn.recv(args.data_buffer)
+        if args.verbose >= 1 :
+            print("Conn {}, addr: {}, Data received: {}".format(nr, addr, data))
+        if len(data) == 0:
+            if time() - last_received < args.end_thread_time:
+                continue
+            else:  # End thread if not conected for a while
+                if args.verbose >= 1:
+                    print ("\nDisconnecting session...")
+                break
+        last_received = time()
+        request = json.loads(data)
+        if not "mode" in request:
+            conn.sendall(str.encode('ERROR: No "mode" in request.'))
+            continue
+        if request['mode'] == 'push':
+            ret = push_data(request)
+            conn.sendall(str.encode(ret))
+        elif request['mode'] == 'pull':
+            pull_data(conn, request)
+        elif request['mode'] == 'check':
+            check_data(conn, request)
+        else:
+            conn.sendall(str.encode('ERROR: Value for "mode" unknown.'))
+
+
+if __name__ == '__main__':
+    connr = 0
+    args = parser.parse_args()
+    msgObjects = MsgObjects(args.object_lifetime)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        s.bind((args.host, args.port))
+        s.listen()
+        while True:
+            conn, addr = s.accept()
+            connr += 1
+            t = threading.Thread(target=connection, args=(connr, conn, addr))
+            t.start()
+
+
+
+	
